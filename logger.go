cb36bcdcce9c851979a27385c6eb64ff0da0bbd5
Airton Oliveira <airtonfelipe@gmail.com>
2019-05-16 11:03:57 -0300

    Refactory logger

--- logger.go
+++ logger.go
@@ -6,67 +6,64 @@ import (
 	"os"
 )
 
-type logHandler struct {
+//Filelogger mantém arquivo e metodos para log
+type Filelogger struct {
 	*log.Logger
 	filename string
 	tag      string
 }
 
-//File Logger handler
-var logInstance *logHandler
-
 // StartLog em arquivo.
 //
 // Cria arquivo logger.log ou concatena ao logger.log
-func StartLog() {
-	StartLogWithTag("~/logger.log", "default-tag ")
+func StartLog() (*Filelogger, error) {
+	return StartLogWithTag("~/logger.log", "default-tag ")
 }
 
 // StartLogWithTag em arquivo e tag.
 //
 // Inicia arquivo logger.log ou adiciona ao existente, permite
 // também especificar string tag padrão no arquivo
-func StartLogWithTag(logfile string, tag string) {
+func StartLogWithTag(logfile string, tag string) (*Filelogger, error) {
 
 	var arquivoLog *os.File
 	arquivoLog, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println("Nao foi possivel escrever no arquivo erro1:", err)
+		return nil, fmt.Errorf("Nao foi possivel escrever no arquivo erro1:%s", err.Error())
 	}
 
-	logInstance = &logHandler{
+	logInstance := &Filelogger{
 		Logger:   log.New(arquivoLog, tag, log.Ldate|log.Lmicroseconds),
-		filename: "logger.log",
+		filename: logfile,
 		tag:      tag,
 	}
+	return logInstance, nil
 }
 
 //Warning adiciona nova linha no arquivo de log com rotulo WARNING
-//
 //Warning log line "DATETIME TAG WARNING"
-func Warning(message string) {
-	logInstance.Println("WARNING ", message)
+func (l *Filelogger) Warning(message string) {
+	l.Println("WARNING ", message)
 }
 
 //Info adiciona nova linha no log com rotulo INFO
 //
 //Info log line "DATETIME TAG INFO"
-func Info(params ...interface{}) {
-	logInstance.Println("INFO ", params)
+func (l *Filelogger) Info(params ...interface{}) {
+	l.Println("INFO ", params)
 }
 
 //Error adiciona nova linha no arquivo de log
 //
 //message é inserida no arquivo de log com rotulo ERROR
-func Error(params ...interface{}) {
-	logInstance.Println("ERROR", params) ///"ERROR ", params)
+func (l *Filelogger) Error(params ...interface{}) {
+	l.Println("ERROR", params) ///"ERROR ", params)
 }
 
 //Debug adiciona nova linha no arquivo de log
 //
-//message é inserida no arquivo de log com rotulo DEBUG
 //TODO: adicionar uma configuração por variavel de ambiente
 //que permite ligar/desligar
-func Debug(params ...interface{}) {
-	logInstance.Println("DEBUG ", params)
+func (l *Filelogger) Debug(params ...interface{}) {
+	l.Println("DEBUG ", params)
 }
